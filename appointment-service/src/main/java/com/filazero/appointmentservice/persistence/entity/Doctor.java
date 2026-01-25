93264f9535e7bb5a3b98c7af72c2bd921ba078f0
Guilherme Marques ferreira <redes.guilherme@gmail.com>
2026-01-25 18:03:04 -0300

    fix: manter Doctor.specialty como String

--- appointment-service/src/main/java/com/filazero/appointmentservice/persistence/entity/Doctor.java
+++ appointment-service/src/main/java/com/filazero/appointmentservice/persistence/entity/Doctor.java
@@ -2,9 +2,9 @@ package com.filazero.appointmentservice.persistence.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Column;
